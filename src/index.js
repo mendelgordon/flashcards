03d62e91709d205079562961881d013484a23344
Mendel Gordon <mendelgordons@gmail.com>
2022-10-25 01:16:05 -0400

    react-codemod update-react-imports, use createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./app/App";
 import store from "./app/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
 );
