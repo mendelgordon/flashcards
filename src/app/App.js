0853f56edd20dadf6b83810ec080ffe29cd1a5ec
Mendel Gordon <mendelgordons@gmail.com>
2022-10-25 01:10:54 -0400

    Create repo

---
+++ src/app/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useMatch,
+} from "react-router-dom";
+import NewQuizForm from "../components/NewQuizForm";
+import NewTopicForm from "../components/NewTopicForm";
+import Topics from "../features/topics/Topics";
+import Topic from "../features/topics/Topic";
+import Quiz from "../features/quizzes/Quiz";
+import Quizzes from "../features/quizzes/Quizzes";
+import ROUTES from "./routes";
+
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={ROUTES.topicsRoute()} activeClassName="active">
+              Topics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.quizzesRoute()} activeClassName="active">
+              Quizzes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.newQuizRoute()} activeClassName="active">
+              New Quiz
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/topics">
+          <TopicsRoutes />
+        </Route>
+        <Route path="/quizzes">
+          <QuizRoutes />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+function TopicsRoutes() {
+  let match = useMatch();
+
+  return (
+    <>
+      <Routes>
+        <Route path={`${match.path}/new`}>
+          <NewTopicForm />
+        </Route>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={`${match.path}`}>
+          <Topics />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+function QuizRoutes() {
+  let match = useMatch();
+
+  return (
+    <>
+      <Routes>
+        <Route path={`${match.path}/new`}>
+          <NewQuizForm />
+        </Route>
+        <Route path={`${match.path}/:quizId`}>
+          <Quiz />
+        </Route>
+        <Route path={`${match.path}`}>
+          <Quizzes />
+        </Route>
+      </Routes>
+    </>
+  );
+}
